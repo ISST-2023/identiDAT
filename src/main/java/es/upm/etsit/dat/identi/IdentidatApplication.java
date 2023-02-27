7afe565f6a5b23ed889c909b9a76cb7a50fa95ee
aLVaRoZz01 <alvaro.rosado.g@gmail.com>
2023-02-27 21:57:12 +0100

    Initial commit

---
+++ src/main/java/es/upm/etsit/dat/identi/IdentidatApplication.java
@@ -0,0 +1,13 @@
+package es.upm.etsit.dat.identi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentidatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentidatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/etsit/dat/identi/IdentidatApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.etsit.dat.identi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentidatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
