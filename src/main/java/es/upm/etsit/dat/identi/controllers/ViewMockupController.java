b6c7e135d615d3b95e1ff94a85fd263eee3a7ad5
aLVaRoZz01 <alvaro.rosado.g@gmail.com>
2023-04-09 21:34:56 +0200

    Added backend functionality to list censusMembers in the database

--- src/main/java/es/upm/etsit/dat/identi/controllers/ViewMockupController.java
+++ src/main/java/es/upm/etsit/dat/identi/controllers/ViewMockupController.java
@@ -1,11 +1,19 @@
 package es.upm.etsit.dat.identi.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import es.upm.etsit.dat.identi.dto.CensusMemberDto;
+import es.upm.etsit.dat.identi.service.CensusMemberService;
+
 @Controller
 public class ViewMockupController {
+  @Autowired
+  private CensusMemberService cenMemService;
 
   @GetMapping("favicon.ico")
   String favicon() {
@@ -44,10 +52,8 @@ public class ViewMockupController {
 
   @GetMapping("/admin/profiles")
   public String list(Model model) {
-    
-
-
-
+    List<CensusMemberDto> censusMembers =  cenMemService.getAll();
+    model.addAttribute("censusMembers", censusMembers);
     return "profiles";
   }
 
