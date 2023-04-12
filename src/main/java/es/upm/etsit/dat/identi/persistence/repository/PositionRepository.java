8e3ff3281a2af83a06ff924d8c91753f3019a981
pablofl01 <pablofl01@gmail.com>
2023-04-12 11:34:52 +0200

    Modified position repository

--- src/main/java/es/upm/etsit/dat/identi/persistence/repository/PositionRepository.java
+++ src/main/java/es/upm/etsit/dat/identi/persistence/repository/PositionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import es.upm.etsit.dat.identi.persistence.model.Position;
 
 public interface PositionRepository extends JpaRepository<Position, Long> {
-    
+    public Position findByPosition(String position);
 }
