b8bb7870ec08b1b16e7203fb6582099de787fa45
aLVaRoZz01 <alvaro.rosado.g@gmail.com>
2023-05-04 16:15:25 +0200

    FIxed template links

--- src/main/resources/static/js/admin/adminFunctions.js
+++ src/main/resources/static/js/admin/adminFunctions.js
@@ -15,3 +15,21 @@ $(document).ready(function () {
         },
     });
 });
+
+$(document).ready(function () {
+    $('#tokensTable').DataTable({
+        order: [[0, 'desc']],
+        language: {
+            lengthMenu: 'Elementos por página: _MENU_ ',
+            zeroRecords: 'No se encuentran resultados',
+            info: 'Página _PAGE_ de _PAGES_',
+            infoEmpty: 'No se encuentran resultados',
+            infoFiltered: '',
+            search: 'Buscar:',
+            paginate: {
+                next: ">",
+                previous: "<"
+            }
+        },
+    });
+});
